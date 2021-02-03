b5c91d64d29adbc4877bc562cf3c16887e4b4925
SahilHemnani777 <hemnanisahil777@gmail.com>
2021-02-03 09:13:42 +0530

    meet- 03-02-2021(Text to Speech)

--- app/src/main/java/com/example/phonolite/MainActivity.java
+++ app/src/main/java/com/example/phonolite/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.phonolite;
 
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private TextToSpeech mTTS;
+    private SeekBar sbPitch;
+    private SeekBar sbSpeed;
+
+    private ImageView A;
+    private Button btnListen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sbPitch = findViewById(R.id.seekBarPitch);
+        sbSpeed = findViewById(R.id.seekBarSpeed);
+        btnListen = findViewById(R.id.btnListen);
+        A= findViewById(R.id.imageView4);
+
+        RotateAnimation anim = new RotateAnimation(0f, 350f, 15f, 15f);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setRepeatCount(Animation.RELATIVE_TO_PARENT);
+        anim.setDuration(500);
+
+        A.setAnimation(anim);
+
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        Log.d(TAG, "onInit: mTTS Success");
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+        btnListen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speak();
+            }
+        });
+
+    }
+    private void speak() {
+        String text = "A. for Apple, A. for Ashoka, A. for Airplane";
+        float pitch = (float) sbPitch.getProgress() / 50;
+        if (pitch < 0.1) pitch = 0.1f;
+        float speed = (float) sbSpeed.getProgress() / 50;
+        if (speed < 0.1) speed = 0.1f;
+        mTTS.setPitch(pitch);
+        mTTS.setSpeechRate(speed);
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
